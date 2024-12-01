6a023d6f26a80be8f9d5e737e0b28f62b15c8fcc
Hisham Ahmed <hisham.ahmed@aol.com>
2024-12-01 20:15:48 +0100

    poisson + reformatted code

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 import torch
 from utils.unet import create_model
 import numpy as np
-from measurement import generate_mask, gaussian_noise
+from measurement import generate_mask, gaussian_noise, poission_noise, inpainting
 from loader import dataloader
 import os 
 import matplotlib.pylab as plt
@@ -31,49 +31,92 @@ model = create_model(
 
 print("Model loaded!")
 
+class DPS:
+    def __init__(self, num_timesteps, scale=0.5, noise="gaussian", sigma=0.05, lamb=1, method="inpainting", operator=None):
+        self.num_timesteps = num_timesteps
+        self.scale = scale
+        self.noise = noise
+        self.sigma = sigma
+        self.lamb = lamb
+        scale = 1000 / self.num_timesteps
+        self.betas = torch.linspace(scale * 0.0001, scale * 0.02, num_timesteps, dtype=torch.float64)
+        self.alphas = 1 - self.betas
+        self.alpha_i = torch.cumprod(self.alphas, axis=0)
+        self.alpha_i_1 = torch.cat((torch.tensor([1.0], dtype=torch.float64), self.alpha_i[:-1]))
+        self.alpha_recip = torch.sqrt(1.0 / self.alpha_i)
+        self.alpha_recip_1 = torch.sqrt(1.0 / self.alpha_i - 1)
+        self.x_i_coeff = (torch.sqrt(self.alphas)  * (1-self.alpha_i_1)) / (1-self.alpha_i)
+        self.x_0_coeff = (torch.sqrt(self.alpha_i_1) * self.betas) / (1-self.alpha_i)
+        posterior_var = self.betas * (1.0 - self.alpha_i_1) / (1.0 - self.alpha_i)
+        self.log_post_var = torch.log(torch.cat((posterior_var[1:2], posterior_var[1:])))
+        self.operator = operator
+        self.method = method
+        self.mask = None
 
-def get_betas_alpha(num_timesteps):
-    scale = 1000 / num_timesteps
-    betas = torch.linspace(scale * 0.0001, scale * 0.02, num_timesteps, dtype=torch.float64)
-    alphas = 1 - betas
-    alpha_i = torch.cumprod(alphas, axis=0)
-    alpha_i_1 = torch.cat((torch.tensor([1.0], dtype=torch.float64), alpha_i[:-1]))
-    alpha_recip = torch.sqrt(1.0 / alpha_i)
-    alpha_recip_1 = torch.sqrt(1.0 / alpha_i - 1)
-    return betas, alphas, alpha_i, alpha_i_1, alpha_recip, alpha_recip_1
+    def get_range_var(self, var, t):
+        f = (var + 1.0) / 2.0
+        mini = self.log_post_var.to(device)[t]
+        maxi = torch.log(self.betas).to(device)[t]
+        range_var = f * maxi + (1-f) * mini
+        return range_var
 
-def get_coeff(betas, alphas, alpha_i, alpha_i_1):
-    x_i_coeff = (torch.sqrt(alphas)  * (1-alpha_i_1)) / (1-alpha_i)
-    x_0_coeff = (torch.sqrt(alpha_i_1) * betas) / (1-alpha_i)
-    return x_i_coeff, x_0_coeff
-
-def get_x_0(x, s, t, alpha_i, alpha_recip, alpha_recip_1):
-    # From paper:
-    # alpha_recip = alpha_recip.to(device)[t]
-    # alpha_i = alpha_i.to(device)[t]
+    def get_x_0(self, x, s, t):
+        # From paper:
+        # alpha_recip = self.alpha_recip.to(device)[t]
+        # alpha_i = self.alpha_i.to(device)[t]
+        
+        # return alpha_recip * (x + (1-alpha_i)*s)
+        alpha_recip = self.alpha_recip.to(device)[t]
+        alpha_recip_1 = self.alpha_recip_1.to(device)[t]
+        return alpha_recip * x - alpha_recip_1 * s 
     
-    # return alpha_recip * (x + (1-alpha_i)*s)
-    alpha_recip = alpha_recip.to(device)[t]
-    alpha_recip_1 = alpha_recip_1.to(device)[t]
-    return alpha_recip * x - alpha_recip_1 * s 
-    
-def get_x_i_1(x_i, x_0, x_i_coeff, x_0_coeff, var, t):
-    x_i_coeff = x_i_coeff.to(device)[t]
-    x_0_coeff = x_0_coeff.to(device)[t]
-    z = torch.randn_like(x_i)
-    if t != 0:
-        sigma = torch.exp(0.5 * var)
-    else:
-        sigma = 0
-    
-    return x_i * x_i_coeff + x_0_coeff * x_0 + sigma * z
+    def get_x_i_1(self, x_i, x_0, var, t):
+        x_i_coeff = self.x_i_coeff.to(device)[t]
+        x_0_coeff = self.x_0_coeff.to(device)[t]
+        z = torch.randn_like(x_i)
+        if t != 0:
+            sigma = torch.exp(0.5 * var)
+        else:
+            sigma = 0
+        
+        return x_i * x_i_coeff + x_0_coeff * x_0 + sigma * z
 
-def gaussian_dps(y, mask, x_0, x_i, x_i_1):
-    diff_norm = torch.linalg.norm(y - (mask * x_0))
-    norm_grad = torch.autograd.grad(outputs=diff_norm, inputs=x_i)[0]
-    scale = 0.5
-    x_i_1 -= scale * norm_grad 
-    return x_i_1
+    def apply_dps(self, y, x_0, x_i, x_i_1):
+        if self.noise == "gaussian":
+            if self.method != "inpainting": # If it is not inpainting
+                diff_norm = torch.linalg.norm(y - self.operator(x_0))
+            else:
+                diff_norm = torch.linalg.norm(y - (self.mask * x_0))
+            norm_grad = torch.autograd.grad(outputs=diff_norm, inputs=x_i)[0]
+
+        else: # if poisson
+            if self.method != "inpainting": # If it is not inpainting
+                diff_norm = torch.linalg.norm(y - self.operator(x_0)) / y.abs()
+            else:
+                diff_norm = torch.linalg.norm(y - (self.mask * x_0)) / y.abs()
+            norm_grad = torch.autograd.grad(outputs=diff_norm.mean(), inputs=x_i)[0]   
+        x_i_1 -= self.scale * norm_grad 
+        return x_i_1
+    
+    def reverse(self, x, y):
+        x_i = x
+        for i in tqdm(reversed(range(self.num_timesteps)), total=self.num_timesteps):
+            t = torch.tensor([i]).to(device)
+            x_i = x_i.requires_grad_()
+            s = model(x_i.float(), t)
+            mu, var = torch.split(s, split_size_or_sections=x_i.shape[1], dim=1)
+            x_0 = self.get_x_0(x_i, mu, t)
+            x_0 = x_0.clamp(-1, 1)
+            range_var = self.get_range_var(var, t)
+            x_i_1 = self.get_x_i_1(x_i, x_0, range_var, t)
+            x_i_1 = self.apply_dps(y, x_0, x_i, x_i_1)
+            x_i = x_i_1.detach_()
+            # remove if you don't want to print every 10 iterations.
+            if i % 10 == 0:
+                file_path = os.path.join('./results/', f"progress/{str(i).zfill(4)}.png")
+                plt.imsave(file_path, clear_img(x_i))
+        
+        return x_i
 
 # From the paper code:
 def clear_img(x):
@@ -85,55 +128,43 @@ def clear_img(x):
     img /= np.max(img)
     return img
 
-def get_log_post_var(betas, alpha_i, alpha_i_1):
-    posterior_var = betas * (1.0 - alpha_i_1) / (1.0 - alpha_i)
-    log_post_var = torch.log(torch.cat((posterior_var[1:2], posterior_var[1:])))
-    return log_post_var
 
-def get_range_var(betas, log_post_var, var, t):
-    f = (var + 1.0) / 2.0
-    mini = log_post_var.to(device)[t]
-    maxi = torch.log(betas).to(device)[t]
-    range_var = f * maxi + (1-f) * mini
-    return range_var
-
-def reverse(x, y, num_timesteps, mask):
-    x_i = x
-    
-    for i in tqdm(reversed(range(num_timesteps))):
-        t = torch.tensor([i]).to(device)
-        x_i = x_i.requires_grad_()
-        s = model(x_i.float(), t)
-        
-        mu, var = torch.split(s, split_size_or_sections=x_i.shape[1], dim=1)
-        x_0 = get_x_0(x_i, mu, t, alpha_i, alpha_recip, alpha_recip_1)
-        x_0 = x_0.clamp(-1, 1)
-        range_var = get_range_var(betas, log_post_var, var, t)
-        x_i_1 = get_x_i_1(x_i, x_0, x_i_coeff, x_0_coeff, range_var, t)
-        x_i_1 = gaussian_dps(y, mask, x_0, x_i, x_i_1)
-        x_i = x_i_1.detach_()
-        # 
-        if i % 10 == 0:
-            file_path = os.path.join('./results/', f"progress/{str(i).zfill(4)}.png")
-            plt.imsave(file_path, clear_img(x_i))
-    
-    return x_i
-
-num_timesteps = 1000
-betas, alphas, alpha_i, alpha_i_1, alpha_recip, alpha_recip_1 = get_betas_alpha(num_timesteps)
-x_i_coeff, x_0_coeff = get_coeff(betas, alphas, alpha_i, alpha_i_1)
-log_post_var = get_log_post_var(betas, alpha_i, alpha_i_1)
-x = torch.randn([1,3,256,256]).to(device)
 
 def main():
-    for i, X in enumerate(dataloader):
+    # Specifiy measurement method
+    method = "inpainting"
+    if method == "inpainting":
+        operator = inpainting
+    elif method == "":
+        operator = None
+    else:
+        operator = None
+
+    # Specifiy DPS config here
+    dps = DPS(num_timesteps=1000, 
+            scale=0.5, 
+            noise="gaussian",
+            sigma=0.05,
+            lamb=1,
+            method=method,
+            operator=operator)
+
+    x = torch.randn([1,3,256,256]).to(device)
+    for i, X in tqdm(enumerate(dataloader), total=len(dataloader)):
         x = torch.randn([1,3,256,256]).to(device).requires_grad_()
-        mask = generate_mask(X, 256, 128).to(device)
-        y = mask * X.to(device)
-        y = gaussian_noise(y).requires_grad_()
+        if method == "inpainting":
+            dps.mask = generate_mask(X, 256, 128).to(device)
+            y = dps.operator(X.to(device), dps.mask)
+        else:
+            y = dps.operator(X.to(device), dps.mask)
+        
+        if dps.noise == "gaussian":
+            y = gaussian_noise(y, dps.sigma).requires_grad_()
+        else:
+            y = poission_noise(y, dps.lamb).requires_grad_()
         file_path = os.path.join('./results/', f"start/{str(i).zfill(5)}.png")
         plt.imsave(file_path, clear_img(y))
-        result = reverse(x, y, num_timesteps, mask)
+        result = dps.reverse(x, y)
         file_path = os.path.join('./results/', f"final/{str(i).zfill(5)}.png")
         plt.imsave(file_path, clear_img(result))       
 
--- measurement.py
+++ measurement.py
@@ -7,5 +7,14 @@ def generate_mask(X, img_dim, box_size, margin_size=5):
     mask[..., x:x+box_size, y:y+box_size] = 0
     return mask
 
+def inpainting(X, mask):
+    return X * mask
+
 def gaussian_noise(X, sigma=0.05):
     return torch.randn_like(X, device=X.device) * sigma  + X
+
+def poission_noise(X, lamb=1):
+    X = (X + 1.0) / 2.0
+    X = X.clamp(0, 1)
+    X = (torch.poisson(X * 255.0 * lamb) / (255.0 * lamb)) * 2.0 - 1.0
+    return X.clamp(-1, 1)
